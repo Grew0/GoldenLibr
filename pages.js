7864713f4b7ddc51043b559f5fbbf6c8838871df
Grew0 <dan123dan5@mail.ru>
2023-12-04 15:45:16 +0300

    fixed fonts

--- pages.js
+++ pages.js
@@ -4,7 +4,6 @@ let rt = document.currentScript.getAttribute("rt");
 window.onmessage = function(event){
     if(event.data == "from iframe"){
         isLandscape = true;
-        console.log(document.getElementById("back_from_iframe"));
     }
 }
 
@@ -13,19 +12,34 @@ window.top.postMessage("is phone", "*");
 setTimeout(
     ()=>{
         if(!isLandscape){
-            console.log("Ok1");
             
             let a = document.getElementById("back_from_iframe");
-            console.log("Ok2");
             if(a != null) a.href= rt + "Shells.html";
-            console.log("Ok3");
 
 
             let item = document.querySelector("body");
             let item_innerHtml = item.innerHTML;
             item.innerHTML += 
             "<img style='position:fixed; width:100vw; height:100vh; z-index: -10000; left:0px; top:0px' src='" + rt + "paper.jpg'></img>"
-            console.log("Ok4");
+
+            document.querySelector('html').innerHTML += 
+            "<style>\
+h1{font-size: 4em}\
+p{font-size: 2em;}\
+li{font-size: 2em;}\
+.back{font-size: 3em;}\
+.interface{font-size: 3em}\
+</style>";
+
+        }else{
+            document.querySelector('html').innerHTML += 
+            "<style>\
+p{font-size: 25px;}\
+li{font-size: 25px;}\
+.back{font-size: 21px;}\
+</style>";
+
+
         }
     },
     15
